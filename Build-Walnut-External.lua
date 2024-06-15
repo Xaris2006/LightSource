e16be0eb74bec4c52deef4bc38e4801019ee2371
Harrys <Harrys@DESKTOP-BDD8GAM>
2024-06-15 17:45:00 +0300

    update preamake files

--- Build-Walnut-External.lua
+++ Build-Walnut-External.lua
@@ -13,13 +13,13 @@ LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
 Library = {}
 Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
 
---group "Dependencies"
---   include "Walnut/vendor/imgui"
---   include "Walnut/vendor/glfw"
---   include "Walnut/vendor/glad"
---   include "Walnut/vendor/yaml-cpp"
---group ""
---
---group "Core"
---    include "Walnut/Walnut/Build-Walnut.lua"
---group ""
+group "Dependencies"
+   include "Walnut/vendor/imgui"
+   include "Walnut/vendor/glfw"
+   include "Walnut/vendor/glad"
+   include "Walnut/vendor/yaml-cpp"
+group ""
+
+group "Core"
+    include "Walnut/Walnut/Build-Walnut.lua"
+group ""
