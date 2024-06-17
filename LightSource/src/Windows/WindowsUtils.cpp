3d3621643ea08cdb4c45942c92297c61818a7ec1
Harrys <Harrys@DESKTOP-BDD8GAM>
2024-06-17 09:26:33 +0300

    REady for building

---
+++ LightSource/src/Windows/WindowsUtils.cpp
@@ -0,0 +1,64 @@
+#include "WindowsUtils.h"
+
+
+#include <Windows.h>
+#include <commdlg.h>
+
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
+#include "Walnut/Application.h"
+
+namespace Windows::Utils
+{
+	std::string OpenFile(std::string Filter)
+	{
+		OPENFILENAMEA ofn;
+		CHAR szFile[260] = { 0 };
+		CHAR currentDir[256] = { 0 };
+		ZeroMemory(&ofn, sizeof(OPENFILENAME));
+		ofn.lStructSize = sizeof(OPENFILENAME);
+		ofn.hwndOwner = glfwGetWin32Window(Walnut::Application::Get().GetWindowHandle());
+		ofn.lpstrFile = szFile;
+		ofn.nMaxFile = sizeof(szFile);
+		if (GetCurrentDirectoryA(256, currentDir))
+			ofn.lpstrInitialDir = currentDir;
+		ofn.lpstrFilter = Filter.c_str();
+		ofn.nFilterIndex = 1;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+		std::string filepath = "";
+		
+		if (GetOpenFileNameA(&ofn) == TRUE)
+			filepath = ofn.lpstrFile;
+		return filepath;
+	}
+	
+	std::string SaveFile(std::string Filter)
+	{
+		OPENFILENAMEA ofn;
+		CHAR szFile[260] = { 0 };
+		CHAR currentDir[256] = { 0 };
+		ZeroMemory(&ofn, sizeof(OPENFILENAME));
+		ofn.lStructSize = sizeof(OPENFILENAME);
+		ofn.hwndOwner = glfwGetWin32Window(Walnut::Application::Get().GetWindowHandle());
+		ofn.lpstrFile = szFile;
+		ofn.nMaxFile = sizeof(szFile);
+		if (GetCurrentDirectoryA(256, currentDir))
+			ofn.lpstrInitialDir = currentDir;
+		ofn.lpstrFilter = Filter.c_str();
+		ofn.nFilterIndex = 1;
+		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+		// Sets the default extension by extracting it from the filter
+		ofn.lpstrDefExt = "*.pgn";//strchr(Filter.c_str(), '\0') + 1;
+
+		std::string filepath = "";
+
+		if (GetSaveFileNameA(&ofn) == TRUE)
+			filepath = ofn.lpstrFile;
+		return filepath;
+	}
+
+}
---
+++ LightSource/src/Windows/WindowsUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace Windows::Utils
+{
+	std::string OpenFile(std::string Filter);
+
+	std::string SaveFile(std::string Filter);
+}
